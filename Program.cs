abf7df869327f2668dd7954441fea7114dcd10a5
Olazee04 <owoiluzainaboluwatoyin@gmail.com>
2025-05-27 13:15:52 +0100

    Initial commit - Prime number program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Prime numbers between 1 and 100:");
+
+        for (int num = 2; num <= 100; num++)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
